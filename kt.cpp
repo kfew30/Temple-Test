5e157320a369cc2ab85f1cea2991b388dfbfe66d
MasterTemple <54373793+MasterTemple@users.noreply.github.com>
2021-01-20 21:03:52 -0800

    Create kt.cpp
    😋

---
+++ kt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 31; i++){
+        cout << "kfew" << i << endl;
+    }
+    cout << "these are all the katies << endl;
+    return 0;
+
+}
